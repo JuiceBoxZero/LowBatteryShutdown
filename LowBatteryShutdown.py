4397c7b8c30057bd84cc4f191cb33c13403a761a
JuiceBoxZero <36525450+JuiceBoxZero@users.noreply.github.com>
2018-03-23 14:57:44 -0600

    Updated LowBatteryShutdown.py
    corrected indenting on callback function

--- LowBatteryShutdown.py
+++ LowBatteryShutdown.py
@@ -1,9 +1,10 @@
 # LowBatteryShutdown.py
-# Rev 021918
+# Rev 03232018
 # This code is complete and can be implemented as it is to safely shutdown
 # a Rasbperry Pi using the safe shut down feature on Juice Box Zero.
 # This is all Raspberry Pi 101, so it is definitely all open source. Feel
 # free to mix, copy, paste, change, etc.
+# There is also no warranty provided of any kind with this code.
 
 import os
 import RPi.GPIO as GPIO
@@ -26,12 +27,12 @@ GPIO.setup(shutdown_pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 def shutdown_callback_function( shutdown_pin ):
 
-# uncomment the following line to have the Pi tell you that the pin is
-# HIGH and that the callback function has been entered. This is mostly
-# useful for debugging.
-#print("the low battery pin is HIGH now, shutting down.")
-
-os.system("sudo shutdown -h now")
+    # uncomment the following line to have the Pi tell you that the pin is
+    # HIGH and that the callback function has been entered. This is mostly
+    # useful for debugging.
+    #print("the low battery pin is HIGH now, shutting down.")
+    
+    os.system("sudo shutdown -h now")
 
 # This is the magic line that adds pin 16 so it is always being watched.
 GPIO.add_event_detect(shutdown_pin, GPIO.RISING, callback=shutdown_callback_function)
