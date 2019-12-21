57f3545251453a8f380177d3c2f9fb3415dffc91
Sander Huijsen <sander.huijsen@gmail.com>
2019-12-21 19:16:13 +0800

    Changed layout of comments.

--- LowBatteryShutdown.py
+++ LowBatteryShutdown.py
@@ -11,17 +11,15 @@ import time
 import RPi.GPIO as GPIO
 
 # This is going to let us use the BCM pin numbers.  The number on JuiceBox
-# Zero is labeled
-# according to BCM.  See https://pinout.xyz/ for more details on pinouts.
+# Zero is labeled according to BCM.  See https://pinout.xyz/ for more
+# details on pinouts.
 
 GPIO.setmode(GPIO.BCM)
 
 # This is where you would change the GPIO from pin 16 to 25, if you needed
-# to do so on the
-# Juice Box Zero board itself. Default is GPIO 16. In order to change the
-# hardware, you would
-# need to cut the GPIO 16 trace on the board and make a solder bridge over
-# GPIO 25.
+# to do so on the Juice Box Zero board itself. Default is GPIO 16. In 
+# order to change the hardware, you would need to cut the GPIO 16 trace on
+# the board and make a solder bridge over GPIO 25.
 # See http://www.blog.juiceboxzero.com/ for more details.
 shutdown_pin = 16  # defines pin 16 as the pin we're watching
 GPIO.setup(shutdown_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
